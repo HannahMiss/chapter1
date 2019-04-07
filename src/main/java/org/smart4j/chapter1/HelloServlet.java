ef8dec3c44b16d0b61f716348ce83035d00c7ca5
hannah <459064232@qq.com>
2019-04-07 15:38:59 +0800

    the first time connect to git.

---
+++ src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -0,0 +1,36 @@
+package org.smart4j.chapter1;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet{
+
+    @Override
+    protected void doGet(HttpServletRequest req,HttpServletResponse resp) throws ServletException,IOException{
+
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        req.setAttribute("currentTime",currentTime);
+        req.getRequestDispatcher("/jsp/hello.jsp").forward(req,resp);
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        System.out.println("abcdefg");
+//        this.init();
+    }
+
+    @Override
+    public void destroy(){
+        System.out.println("destroy");
+    }
+}
---
+++ src/main/webapp/jsp/hello.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: hannah
+  Date: 2019/4/3
+  Time: 下午5:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+
+<h1>Hello!</h1>
+
+<h2>当前时间：${currentTime}</h2>
+
+</body>
+</html>
